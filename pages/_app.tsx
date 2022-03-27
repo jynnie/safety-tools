8ec96dddb025f5f5fc256f83f0c7014009c8e43e
jynnie <jynn.tang@gmail.com>
2022-03-26 17:56:45 -0700

    🔥 Set up firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import type { AppProps } from "next/app";
+import { Database, getDatabase } from "firebase/database";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "firebaseConfig";
+
+import "juniper-ui/dist/style.css";
+
+//* Firebase Setup
+let db: Database;
+try {
+  initializeApp(firebaseConfig);
+  db = getDatabase();
+} catch (error: any) {
+  if (!/already exists/u.test(error.message)) {
+    console.error("Firebase admin initialization error", error.stack);
+  }
+}
+export const FirebaseContext: React.Context<{
+  db: Database;
+}> = React.createContext(null as any);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <FirebaseContext.Provider value={{ db }}>
+        <Component {...pageProps} />
+      </FirebaseContext.Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
